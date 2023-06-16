5637ca2c0884b98d14a8385dcd2da033f32a5841
DarkKnightKingBotDISA <136807390+DarkKnightKingBotDISA@users.noreply.github.com>
2023-06-16 16:34:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94717307774', 'Owner', true],
+  ['94717307774', 'Dark Knight King Bot DISA', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'Dark Knight King Bot DISA'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
